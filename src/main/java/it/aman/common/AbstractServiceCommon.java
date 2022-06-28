8a16da725ff93b9a4d62e61835ac8ac08e0fce25
amenski <amantwd@gmail.com>
2022-06-28 21:40:56 +0300

    add threeten date mapper

---
+++ src/main/java/it/aman/common/AbstractServiceCommon.java
@@ -0,0 +1,27 @@
+package it.aman.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+
+import it.aman.common.exception.ERPException;
+import it.aman.common.exception.ERPExceptionEnums;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AbstractServiceCommon {
+
+    private final HttpServletRequest request;
+    
+    public String getLoggedInUserFromHeader() throws ERPException {
+        final String userName = request.getHeader(ERPConstants.X_REQUESTED_URL_SUBJECT);
+        if (StringUtils.isBlank(userName)) {
+            log.error("User name not found in the header.");
+            throw ERPExceptionEnums.UNAUTHORIZED_EXCEPTION.get();
+        }
+        return userName;
+    }
+}
---
+++ src/main/java/it/aman/common/DateMapper.java
@@ -0,0 +1,25 @@
+package it.aman.common;
+
+import org.springframework.stereotype.Component;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.OffsetDateTime;
+
+@Component
+public class DateMapper {
+
+    public java.time.LocalDate toJavaTimeLocalDate(LocalDate value) {
+        return java.time.LocalDate.parse(value.toString());
+    }
+    
+    public java.time.OffsetDateTime toJavaTimeOffsetDateTime(OffsetDateTime value) {
+        return java.time.OffsetDateTime.parse(value.toString());
+    }
+    
+    public LocalDate toThreetenLocalDate(java.time.LocalDate value) {
+        return LocalDate.parse(value.toString());
+    }
+    
+    public OffsetDateTime toThreetenOffsetDateTime(java.time.OffsetDateTime value) {
+        return OffsetDateTime.parse(value.toString());
+    }
+}
